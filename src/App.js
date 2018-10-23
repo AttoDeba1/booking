cc62643cda81e1a500124dac6f30b94d5c0db3b1
ATTO DEBA <ads.attodeba@gmail.com>
2018-10-23 22:26:32 +0200

    added bootstrap and Jquery dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from "./template/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <HomePage/>
+
         <header className="App-header">
+            <div className="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+
           <img src={logo} className="App-logo" alt="logo" />
+          <div className={"success"}> ddddddddddddddddd</div>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -20,6 +28,29 @@ class App extends Component {
             Learn React
           </a>
         </header>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+              Launch demo modal
+          </button>
+
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          ...
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
     );
   }
