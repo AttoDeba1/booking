1e54d489d7243da04d752ba846aa197ad06cb344
ATTO DEBA <ads.attodeba@gmail.com>
2018-10-23 22:29:07 +0200

    added HomePage's component

---
+++ src/template/HomePage.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+class HomePage extends Component{
+    constructor(props){
+        super(props);
+}
+render(){
+        return(
+         <div className={"container"}>
+             {"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"}
+        </div>);
+}
+}
+export default HomePage;
